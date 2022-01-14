b2a8bf61152079828178b8edc23e2f199312f1d6
pedparbas1 <pparrilla10@gmail.com>
2022-01-14 12:27:47 +0100

    5hechos

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,18 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +21,26 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+@Entity
+public class Feeding extends BaseEntity {
+    @NotNull
+    @Column(name = "start_date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
     LocalDate startDate;
+
+    @NotNull
+    @Min(1)
+    @Column(name = "weeks_duration")
     double weeksDuration;
-    Pet pet;   
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    Pet pet;  
+    
+    
+    @ManyToOne
+    @JoinColumn(name = "feeding_type")
+    FeedingType feedingType; 
+    
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
@@ -3,9 +3,20 @@ package org.springframework.samples.petclinic.feeding;
 import java.util.List;
 import java.util.Optional;
 
-public interface FeedingRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedingRepository  extends CrudRepository<Feeding, Integer> {
     List<Feeding> findAll();
-    // List<FeedingType> findAllFeedingTypes();
+
+    @Query("SELECT f FROM FeedingType f")
+    List<FeedingType> findAllFeedingTypes();
+
+   /*  @Query("SELECT f FROM FeedingType p WHERE p.name=?1")
+    FeedingType findFeedingType(String name); */
+
     Optional<Feeding> findById(int id);
     Feeding save(Feeding p);
 }
